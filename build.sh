f68e6f7ea8f1fcb2e30e6886d2044dc641923d22
Mikuláš Dítě <rullaf@gmail.com>
2017-03-03 13:06:58 +0100

    remove old archives in buil

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR"
+rm -rf "$DIR/dist/build/distributions/"gumtree-*-2.1.0-SNAPSHOT.zip
 
 set -x
 ./gradlew build -x test
